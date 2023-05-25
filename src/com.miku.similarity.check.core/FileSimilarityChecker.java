fde5900454321b56d1bbe2607e2961772462dbef
gemingjia <gmj2334404686@163.com>
2023-05-25 13:27:12 +0800

    添加了注释，优化了部分代码

--- src/com.miku.similarity.check.core/FileSimilarityChecker.java
+++ src/com.miku.similarity.check.core/FileSimilarityChecker.java
@@ -12,13 +12,18 @@ import java.util.Map;
  */
 public class FileSimilarityChecker {
 
+    // 文件1的路径
     private static final String FILE1_PATH = "targetfile/1.txt";
+    // 文件2的路径
     private static final String FILE2_PATH = "targetfile/2.txt";
 
     public static void main(String[] args) {
         try {
+            // 读取文件1的内容
             String file1Content = readFileContent(FILE1_PATH);
+            // 读取文件2的内容
             String file2Content = readFileContent(FILE2_PATH);
+            // 计算文件相似度
             double similarity = calculateSimilarity(file1Content, file2Content);
             System.out.println("文件相似度: " + similarity);
         } catch (IOException e) {
@@ -26,40 +31,44 @@ public class FileSimilarityChecker {
         }
     }
 
-
     // 读取文件内容
     private static String readFileContent(String filePath) throws IOException {
         StringBuilder content = new StringBuilder();
 
+        // 使用BufferedReader逐行读取文件内容
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                content.append(line);
-                content.append("\n");
+                content.append(line).append("\n");
             }
         }
 
         return content.toString();
     }
 
-
     // 计算文件相似度
     private static double calculateSimilarity(String content1, String content2) {
+        // 获取文本内容的词频向量
         Map<String, Integer> vector1 = getTermFrequency(content1);
         Map<String, Integer> vector2 = getTermFrequency(content2);
 
+        // 计算向量的点积
         double dotProduct = calculateDotProduct(vector1, vector2);
+        // 计算向量的模
         double magnitude1 = calculateMagnitude(vector1);
         double magnitude2 = calculateMagnitude(vector2);
 
+        // 计算相似度
         return dotProduct / (magnitude1 * magnitude2);
     }
 
     // 计算词频
     private static Map<String, Integer> getTermFrequency(String content) {
         Map<String, Integer> termFrequency = new HashMap<>();
+        // 将文本内容按空白字符分割为单词数组
         String[] words = content.split("\\s+");
 
+        // 统计每个单词的出现频率
         for (String word : words) {
             termFrequency.put(word, termFrequency.getOrDefault(word, 0) + 1);
         }
@@ -67,8 +76,7 @@ public class FileSimilarityChecker {
         return termFrequency;
     }
 
-
-    // 计算点积
+    // 计算向量的点积
     private static double calculateDotProduct(Map<String, Integer> vector1, Map<String, Integer> vector2) {
         double dotProduct = 0;
 
@@ -76,6 +84,7 @@ public class FileSimilarityChecker {
         Map<String, Integer> smallerVector = vector1.size() < vector2.size() ? vector1 : vector2;
         Map<String, Integer> largerVector = vector1.size() >= vector2.size() ? vector1 : vector2;
 
+        // 计算点积
         for (Map.Entry<String, Integer> entry : smallerVector.entrySet()) {
             String term = entry.getKey();
             int frequency1 = entry.getValue();
@@ -87,15 +96,17 @@ public class FileSimilarityChecker {
         return dotProduct;
     }
 
-
     // 计算向量的模
     private static double calculateMagnitude(Map<String, Integer> vector) {
         double magnitudeSquared = 0;
 
+        // 计算向量模的平方
         for (int frequency : vector.values()) {
             magnitudeSquared += frequency * frequency;
         }
 
+        // 对模的平方进行开方，得到模的值
         return Math.sqrt(magnitudeSquared);
     }
 }
+
